ef8e9edbacdc854decbac349390bfe5a16b77128
Lukasz Klekner <lukaszklekner@gmail.com>
2021-02-27 21:49:18 +0100

    Day#19: added effect functions to scripts (wesbos's functions) and uncommented inputs in index.html file

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,12 +24,13 @@ function drawOntoCanvas(){
     canvas.height = height
 
     ctx.drawImage(video, 0, 0, width, height)
+
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
+
     requestAnimationFrame(drawOntoCanvas)
 }
-
-getMedia().catch(err => {
-    console.error(err);
-});
 
 function takePhoto(){
     snap.currentTime=0;
@@ -45,4 +46,51 @@ function takePhoto(){
     strip.insertAdjacentElement('afterbegin', link);
 }
 
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] + 200;
+      pixels.data[i + 1] = pixels.data[i + 1] - 50;
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+  }
+
+  function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i - 150] = pixels.data[i + 0];
+      pixels.data[i + 500] = pixels.data[i + 1];
+      pixels.data[i - 550] = pixels.data[i + 2];
+    }
+    return pixels;
+  }
+
+  function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        pixels.data[i + 3] = 0;
+      }
+    }
+
+    return pixels;
+  }
+
+getMedia().catch(err => {
+    console.error(err);
+});
 video.addEventListener('canplay', drawOntoCanvas);
